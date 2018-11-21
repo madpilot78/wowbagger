568fb5eda507823758705bf744522ae7d621ef31
Guido Falsi <mad@madpilot.net>
2018-11-21 17:06:39 +0100

    Import files to create a bare bones phpunit test suite.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace madpilot78\wowbagger\tests;
+
+class ExaampleTest extends \PHPUnit\Framework\TestCase
+{
+	/**
+     * Simple example test.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
